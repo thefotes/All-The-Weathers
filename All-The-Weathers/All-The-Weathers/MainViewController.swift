a4f44b684b0f30f83eed1c37af31943017026e6c
Peter Foti <peterfoti@me.com>
2015-10-19 17:43:45 -0400

    Cleans up inital commit, adds nav controller w/ title

--- All-The-Weathers/All-The-Weathers/MainViewController.swift
+++ All-The-Weathers/All-The-Weathers/MainViewController.swift
@@ -1,12 +1,10 @@
-//  AppDelegate.swift
+//  ViewController.swift
 //  All-The-Weathers
 //  Created by Peter Foti on 10/19/15.
 //  Copyright © 2015 Peter Foti. All rights reserved.
 
 import UIKit
 
-@UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
-    var window: UIWindow?
+final class MainViewController: UIViewController {
+    
 }
