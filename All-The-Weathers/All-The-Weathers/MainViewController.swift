f76c62e9c8b79f0eb9d73b4297e8cf785e678869
Peter Foti <peterfoti@me.com>
2015-10-19 19:04:36 -0400

    Updates users current city in main vc on load.

--- All-The-Weathers/All-The-Weathers/MainViewController.swift
+++ All-The-Weathers/All-The-Weathers/MainViewController.swift
@@ -7,4 +7,28 @@ import UIKit
 
 final class MainViewController: UIViewController {
     
+    @IBOutlet weak private var cityStateLabel: UILabel!
+    
+    let animationDuration: NSTimeInterval = 0.8
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        LocationManager.sharedInstance.addObserver(self, forKeyPath: "currentLocation", options: [.New, .Old], context: nil)
+        LocationManager.sharedInstance.updateLocation()
+        cityStateLabel.animateTextUpdate("Updating Current Location", animationDuration: animationDuration)
+    }
+    
+    func configureWithLocation(currentLocation: Location) {
+        cityStateLabel.animateTextUpdate(currentLocation.cityAndState, animationDuration: animationDuration)
+    }
+    
+    deinit {
+        LocationManager.sharedInstance.removeObserver(self, forKeyPath: "currentLocation")
+    }
+    
+    override func observeValueForKeyPath(keyPath: String?, ofObject object: AnyObject?, change: [String : AnyObject]?, context: UnsafeMutablePointer<Void>) {
+        if let change = change {
+            configureWithLocation(change["new"] as! Location)
+        }
+    }
 }
