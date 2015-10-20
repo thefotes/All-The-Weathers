a4e9042927897c3cd24c92543ee6e3cba0bfac34
Peter Foti <peterfoti@me.com>
2015-10-19 21:30:13 -0400

    Moves table view delegate and data source out of main vc.

--- All-The-Weathers/All-The-Weathers/MainViewController.swift
+++ All-The-Weathers/All-The-Weathers/MainViewController.swift
@@ -9,21 +9,31 @@ final class MainViewController: UIViewController {
     
     @IBOutlet weak private var cityStateLabel: UILabel!
     @IBOutlet weak private var currentTempLabel: UILabel!
+    @IBOutlet weak private var tableView: UITableView!
     
     let animationDuration: NSTimeInterval = 0.8
+    let tableViewDataSource = MainTableViewDataSource()
+    let tableViewDelegate = MainTableViewDelegate()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         LocationManager.sharedInstance.addObserver(self, forKeyPath: "currentLocation", options: [.New, .Old], context: nil)
         LocationManager.sharedInstance.updateLocation()
         cityStateLabel.animateTextUpdate("Updating Current Location", animationDuration: animationDuration)
+        let nib = UINib(nibName: .WeatherTableViewCell)
+        tableView.registerNib(nib, forCellReuseIdentifier: NibNames.WeatherTableViewCell.rawValue)
+        tableView.dataSource = tableViewDataSource
+        tableView.delegate = tableViewDelegate
+        tableView.allowsSelection = false
     }
     
     func configureWithLocation(currentLocation: Location) {
         cityStateLabel.animateTextUpdate(currentLocation.cityAndState, animationDuration: animationDuration)
         OpenWeatherMapNetworkCommunicator.sharedInstance.fetchCurrentWeather(currentLocation) { (weatherObjects) -> Void in
+            self.tableViewDataSource.weatherData = weatherObjects!
             dispatch_async(dispatch_get_main_queue(), { () -> Void in
                 self.currentTempLabel.animateTextUpdate(weatherObjects!.last!.temperatureString, animationDuration: self.animationDuration)
+                self.tableView.reloadData()
             })
         }
         
